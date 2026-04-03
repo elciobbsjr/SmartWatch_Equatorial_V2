cb989f8b869c432701f8e5bbd12a64bd695381c5
elciobsjr <elcio.junior@discente.ufma.br>
2026-04-03 14:45:03 -0300

    Primeiro commit - SmartWatch Equatorial V2

---
+++ .pio/build/lolin_c3_mini/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/lolin_c3_mini/FrameworkArduino/libb64/cdecode.c.o: \
+ C:/Users/ResTIC16/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ C:/Users/ResTIC16/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
